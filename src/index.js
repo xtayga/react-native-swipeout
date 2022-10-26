b09971ce20116f459a2dd7d9a0003c13f5ec5cb9
ioer <ioer@izn.io>
2022-10-26 11:20:29 +0800

    chore: 处理ViewPropTypes被移除的问题

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import React, {
 } from 'react';
 import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
+import {ViewPropTypes} from 'deprecated-react-native-prop-types'
 
 import {
   PanResponder,
@@ -14,7 +15,6 @@ import {
   StyleSheet,
   Text,
   View,
-  ViewPropTypes,
 } from 'react-native';
 
 const SwipeoutBtn = createReactClass({
